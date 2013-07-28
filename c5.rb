366c1a286ffbd8fbdce7fde518c4273a3f0b4408
akaSocrates <janusjavier@gmail.com>
2013-07-27 22:53:57 -0700

    initial commit

---
+++ c5.rb
@@ -0,0 +1,12 @@
+puts 'Hi what is your first name?'
+first_name = gets.chomp
+puts 'That\'s such a cool name! What\'s your last name?'
+last_name = gets.chomp
+puts 'What\'s your middle name?'
+middle_name = gets.chomp
+puts 'Well it was great to meet you, ' + first_name + ' ' + middle_name + ' ' + last_name + '!'
+
+puts 'OK, so what\'s your favorite number?'
+fave_num = gets.chomp
+bigger_num = fave_num.to_i + 1
+puts 'Well, you know that\'s a good number and all. But ' + bigger_num.to_s + ' is a betterer number!'
---
+++ calc.rb
@@ -0,0 +1,82 @@
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+puts 9 / 2
+
+puts 5 * (12-8) + -15
+puts 98 + (59872 / (13*8) * -51)
+
+puts 24 * 365
+
+puts 10 * 365 * 24 * 60
+
+puts 40 * 365 * 24 * 60 * 60
+
+puts 1160000000 / (60 * 60 * 24 * 365) 
+
+
+puts 'Hello World'
+puts ' '
+puts 'Good-bye.'
+
+puts 'I like' + ' apple pie.'
+
+puts 'blink ' * 4
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+puts 'You\'re swell.'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
+
+my_string = 'you can say that again...'
+
+puts my_string
+puts my_string
+
+name = 'Juan de la Cruz'
+puts 'My name is ' + name + '.'
+puts 'Wow! ' + name + ' is a really long name.'
+
+composer = 'Bach'
+puts composer + ' was "da bomb" in his day.'
+
+composer = 'Mozart'
+puts 'But I prefer ' + composer + ', personally.'
+
+newvar = 'California'
+puts newvar
+
+newvar = 5 * (1 + 2)
+puts newvar
+
+var1 = 8
+var2 = var1
+
+puts var1
+puts var2
+
+var1 = 'eight'
+
+puts var1
+puts var2
+
+var1 = 2
+var2 = '5'
+puts var1.to_s + var2
+
+puts var1 + var2.to_i
+
+puts gets
+Is there an echo?
+
+
