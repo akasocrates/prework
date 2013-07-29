65e352ba3333c54096346a58e72701d334202442
akaSocrates <janusjavier@gmail.com>
2013-07-29 16:59:57 -0700

    added Chapter 6 exercises

---
+++ boss.rb
@@ -0,0 +1,7 @@
+puts 'What do you want?!'
+reply = gets.chomp
+puts 'Are you kidding me? ' + reply + '!' + ' You\'re fired!'
+
+
+line_width = 100
+puts 'Chapter 1: Getting Started'.ljust(line_width/2) + 'page 1'.rjust(line_width/2)
